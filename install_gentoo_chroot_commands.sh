28082834349096b4174b89b759b11c326f784bc4
Nicolas Keita <nicolaskeita2@gmail.com>
2024-03-31 21:11:02 +0200

    initial_commit

--- install_gentoo_chroot_commands.sh
+++ install_gentoo_chroot_commands.sh
@@ -3,6 +3,9 @@
 source /etc/profile
 export PS1="(chroot) $PS1"
 
-emerge-webrsync
+emerge-webrsync --quiet
 emerge --sync --quiet
+
+emerge --oneshot --quiet app-portage/cpuid2cpuflags
+echo "*/* $(cpuid2cpuflags)" > /etc/portage/package.use/00cpu-flags
 
